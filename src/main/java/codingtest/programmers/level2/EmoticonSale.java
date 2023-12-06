8fe138b1a0f6f136c2a80a0af9a27a8d0ea02b4c
space <enaenen@naver.com>
2023-12-06 10:57:42 +0900

    x

---
+++ src/main/java/codingtest/programmers/level2/EmoticonSale.java
@@ -0,0 +1,25 @@
+package codingtest.programmers.level2;
+//https://school.programmers.co.kr/learn/courses/30/lessons/150368
+public class EmoticonSale {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int[] solution(int[][] users, int[] emoticons) {
+		int[] answer = new int[2];
+		int[] discountRate = new int[] {10,20,30,40};
+		int[] arr = new int[emoticons.length];
+		for (int i =0; i< users.length; i++){
+			for (int j = 0; j < discountRate.length; j++){
+
+			}
+
+		}
+
+
+		return answer;
+	}
+
+
+}
