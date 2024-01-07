97022a4e3bafdd4077b32301ec8b306660798c64
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-01-07 11:03:22 +0900

    [Silver II] Title: 에디터, Time: 492 ms, Memory: 59416 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String inputWord = br.readLine();
+
+        Stack<Character> leftFromCursor = new Stack<>();
+        Stack<Character> rightFromCursor = new Stack<>();
+
+        char[] charArray = inputWord.toCharArray();
+        for (char character : charArray) {
+            leftFromCursor.add(character);
+        }
+
+        int commandCounter = Integer.parseInt(br.readLine());
+        while(commandCounter-->0){
+            String command = br.readLine();
+            char[] commandChar = command.toCharArray();
+            if(commandChar[0]=='P'){
+                char add = commandChar[2];
+                leftFromCursor.add(add);
+            }
+            else if(commandChar[0]=='L'){
+                if(leftFromCursor.empty()){
+                    continue;
+                }else{
+                    rightFromCursor.add(leftFromCursor.pop());
+                }
+            }
+            else if(commandChar[0]=='D'){
+                if(rightFromCursor.empty()){
+                    continue;
+                }else{
+                    leftFromCursor.add(rightFromCursor.pop());
+                }
+            }
+            else if(commandChar[0]=='B'){
+                if(leftFromCursor.empty())continue;
+                else{
+                    leftFromCursor.pop();
+                }
+            }
+        }
+        for (Character character : leftFromCursor) {
+            bw.write(character);
+        }
+        while(!rightFromCursor.empty()){//오른쪽은 거꾸로 저장되어있기때문에 pop으로 꺼냄
+            bw.write(rightFromCursor.pop());
+        }
+        bw.flush();
+        bw.close();
+    }
+}
