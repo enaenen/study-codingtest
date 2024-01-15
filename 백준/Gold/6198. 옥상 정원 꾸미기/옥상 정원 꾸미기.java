44459ff400fcd24f238edcc799cf4488d453fb6a
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-01-15 19:32:30 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 300 ms, Memory: 24624 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long buildings = Long.parseLong(br.readLine());
+        Stack<Long> stack = new Stack<>();
+        long total = 0;
+        while (buildings-- > 0){
+            long current = Long.parseLong(br.readLine());
+            while (!stack.isEmpty() && stack.peek() <= current){
+                stack.pop();
+            }
+            total += stack.size();
+            stack.push(current);
+        }
+        System.out.println(total);
+    }
+}
