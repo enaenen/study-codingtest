a3c7dc7fae630027bfaf2b64be4d1aef48e30c98
space <enaenen@naver.com>
2024-02-17 23:25:28 +0900

    Recursion

---
+++ src/main/java/codingtest/basic/recursion/HanoiTower.java
@@ -0,0 +1,29 @@
+package codingtest.basic.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HanoiTower {
+
+	private static final StringBuilder sb = new StringBuilder();
+	private static int counter;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		solution(Integer.parseInt(br.readLine()), 1, 2, 3);
+		System.out.println(counter);
+		System.out.println(sb.toString());
+	}
+
+	public static void solution(int n, int start, int middle, int end) {
+		counter++;
+		if (n == 1) {
+			sb.append(start + " " + end + "\n");
+		} else {
+			solution(n - 1, start, end, middle);
+			sb.append(start + " " + end + "\n");
+			solution(n - 1, middle, start, end);
+		}
+	}
+}
