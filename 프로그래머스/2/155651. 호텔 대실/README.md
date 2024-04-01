6bd6207d3ea2571bf896cc9a692c34099a2ce500
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-04-01 21:15:41 +0900

    [level 2] Title: 호텔 대실, Time: 6.46 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 3.36 ms
+메모리: 81.6 MB, 시간: 6.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 20:06:16
+2024년 04월 01일 21:15:39
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,23 +1,33 @@
 import java.util.*;
 
 class Solution {
+    public void print(int[][] arr){
+        for (int[] a : arr){
+            for(int n : a){
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
     public int solution(String[][] book_time) {
-        int[][] times = new int[book_time.length][2];
-        for (int i = 0; i < book_time.length; i++) {
-            times[i][0] = Integer.parseInt(book_time[i][0].substring(0, 2)) 
-                * 60 + Integer.parseInt(book_time[i][0].substring(3, 5));
-            times[i][1] = Integer.parseInt(book_time[i][1].substring(0, 2)) 
-                * 60 + Integer.parseInt(book_time[i][1].substring(3, 5)) + 10;
-        }
-        Arrays.sort(times, (a, b) -> a[0] - b[0]);
-        // System.out.println(Arrays.deepToString(times));
+        int[][] bookTimes = new int[book_time.length][2];
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(times[0][1]);
-        for (int i = 1; i < times.length; i++) {
-            if (pq.peek() <= times[i][0]) pq.poll();
-            pq.add(times[i][1]);
+        for (int i = 0; i < book_time.length; i++){
+            String[] inTime = book_time[i][0].split(":");
+            String[] outTime = book_time[i][1].split(":");
+           
+            bookTimes[i][0] = (Integer.parseInt(inTime[0])) * 60 + (Integer.parseInt(inTime[1]));
+            bookTimes[i][1] = (Integer.parseInt(outTime[0])) * 60 + (Integer.parseInt(outTime[1])) + 10;
+            
         }
-        return pq.size();
+        
+        Arrays.sort(bookTimes, (o1, o2) -> o1[0] - o2[0]);
+        PriorityQueue<Integer> outTime = new PriorityQueue<>();
+        outTime.add(bookTimes[0][1]);
+        for (int i = 1; i < bookTimes.length; i++){
+            if (outTime.peek() <= bookTimes[i][0]) outTime.poll();
+            outTime.add(bookTimes[i][1]);
+        }
+        return outTime.size();
     }
 }
