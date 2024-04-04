146701774199690f1d605c9712d2ed23984aee8f
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-04-04 22:42:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        if (n == 2){
+            return 1;
+        }
+        
+        int answer = 0;
+        
+        while (a != b){
+            a = (a+1) / 2;
+            b = (b+1) / 2;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
