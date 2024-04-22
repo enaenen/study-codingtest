ddcd6df43babb99875a764adfef84f344d329c4c
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-04-22 22:15:36 +0900

    Time: 2 ms (70.48%), Space: 43.7 MB (96.45%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) -> { return o1[2] - o2[2]; });
-        Arrays.sort(trips, (o1, o2) -> {return o1[1] - o2[1];});
-        int passenger = 0;
-        
-        for (int i = 0; i < trips.length; i++){
-            int[] current = trips[i];
-            passenger += current[0];
+        int[] map = new int[1001];
 
-            while (!que.isEmpty() && que.peek()[2] <= current[1]){
-                passenger -= que.poll()[0];
-            }
-            
-            if (passenger > capacity){
+        for (int[] trip : trips){
+            map[trip[1]] += trip[0];
+            map[trip[2]] -= trip[0];
+        }
+
+        int passengers = 0;
+        for (int road : map){
+            passengers += road;
+            if (capacity < passengers)
                 return false;
-            }
-            que.offer(current);
         }
         return true;
     }
