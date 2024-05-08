853430ec78db70ceb2e7bf3e3d9fcd056a5de4dd
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-05-09 07:47:40 +0900

    Time: 13 ms (41.51%), Space: 45.2 MB (17.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
-        return s.equals(new StringBuilder(s).reverse().toString());
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        return new StringBuilder(s).reverse().toString().equals(s);
     }
 }
