baaac7240b98d92e6ecc0476211a37fe80591be1
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-05-17 01:59:07 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (73.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -19,17 +19,19 @@ class Solution {
 			if (nums[start] == target) {
 				return start;
 			}
-			if (target < nums[mid]) {
-				if (nums[end] < nums[mid] && target <= nums[end]) {
-					start = mid;
+			if (target < nums[mid]) { // 타겟이 중간값보다 작은경우
+				if (nums[end] < nums[mid] && target <= nums[end]) { 
+                    // 뒤집힌경우(4 5 6 1 2 3) 가운데가 끝 포인터보다 더 큰경우 
+                    // 타겟이 mid 기준으로 뒤쪽 어딘가 있는경우
+					start = mid; // 기존의 이분탐색방법대로가 아닌 start를 중간지점으로
 				} else {
 					end = mid;
 				}
 			} else {
-				if (nums[mid] < nums[start] && nums[start] < target) {
-					end = mid;
+				if (nums[mid] < nums[start] && nums[start] <= target) { //뒤집힌경우 + 타겟이 mid 기준 앞쪽 어딘가 있는경우
+					end = mid;//앞쪽에서 찾기
 				} else {
-					start = mid;
+					start = mid;//뒤쪽에서 찾기
 				}
 			}
 			mid = (start + end) / 2;
