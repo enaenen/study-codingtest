d50de3ec7538d9cb45574c961f501d51171bb837
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-06-02 22:33:34 +0900

    Time: 1 ms (99.75%), Space: 55.2 MB (91.74%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int total = 0;
+        int arrayCount = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+
+        while (end < nums.length){
+            total += nums[end];
+            if (total < target){
+                end++;
+            }
+            else {
+                while (target <= total){
+                    arrayCount = Math.min(arrayCount, end - start + 1);
+                    total -= nums[start++];
+                }
+                end++;
+            }
+        }
+        if (start == 0) return 0;
+        return arrayCount;
+
+    }
+}
