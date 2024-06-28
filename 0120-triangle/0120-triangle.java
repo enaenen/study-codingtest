8a648efcd44248e4a95ee7218618ff526c0286bc
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-06-29 07:24:46 +0900

    Time: 1 ms (99.4%), Space: 44.4 MB (25.69%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,22 +1,18 @@
 class Solution {
-    static final int MAX_TOTAL = 10001;
     public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 1) return triangle.get(0).get(0);
         int[][] dp = new int[triangle.size()][triangle.size()];
+        for (int[] arr : dp){
+            Arrays.fill(arr, 10001);
+        }
+        return goingDown(triangle, dp, 0, 0);
         
-        for(int[] arr : dp){
-            Arrays.fill(arr, MAX_TOTAL);
-        }
-        return calc(triangle, 0, 0, dp);
     }
-    private static int calc(List<List<Integer>> triangle, int i, int j, int[][] dp){
-        if (i == triangle.size()){
-            return 0;
-        }
-        if (dp[i][j] != MAX_TOTAL){
-            return dp[i][j];
-        }
-        int left = triangle.get(i).get(j) + calc(triangle, i+1, j, dp);
-        int right = triangle.get(i).get(j) + calc(triangle, i+1, j+1, dp);
+    public int goingDown(List<List<Integer>> triangle, int[][] dp, int i, int j){
+        if (i == triangle.size()) return 0;
+        if (dp[i][j] != 10001) return dp[i][j];
+        int left = triangle.get(i).get(j) + goingDown(triangle, dp, i + 1, j);
+        int right = triangle.get(i).get(j) + goingDown(triangle, dp, i + 1, j + 1);
         return dp[i][j] = Math.min(left, right);
     }
 }
