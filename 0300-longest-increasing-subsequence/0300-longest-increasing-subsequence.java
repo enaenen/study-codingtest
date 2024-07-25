6c0f61ff4e1c927510780ec108c25dbb3760ae39
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-07-25 18:39:52 +0900

    Time: 7 ms (85.29%), Space: 44.3 MB (36.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,10 +1,9 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-        pq.offer(nums[0]);
-
+        List<Integer> subList = new ArrayList<Integer>();
+        subList.add(nums[0]);
         for (int i = 1; i < nums.length; i++){
-            if (nums[i] > pq.peek()) pq.add(nums[i]);
+            if (nums[i] > subList.get(subList.size() - 1)) subList.add(nums[i]);
             else {
                 // num[i] 보다 같거나 큰 수를 찾음 in subList
                 for (int j = 0; j < subList.size(); j++){
