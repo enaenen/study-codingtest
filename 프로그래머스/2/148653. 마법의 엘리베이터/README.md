356e42b17694cdc7e3ced9c9399cc69605289de9
SpaceChae <13278955+enaenen@users.noreply.github.com>
2024-10-29 07:10:01 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java#) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.03 ms
+메모리: 87.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 29일 06:45:27
+2024년 10월 29일 07:10:00
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,22 +1,21 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-    
         while (storey != 0){
-               int unit = storey % 10;
-            
-            // 올림할지, 내림할지 판단
-            if (unit > 5) {
-                answer += 10 - unit;  // 올림하여 10의 자리로 넘김
-                storey += 10;
-            } else if (unit == 5) {
-                // 다음 자리가 5 이상인 경우에는 올림, 그렇지 않으면 그대로 처리
-                if ((storey / 10) % 10 >= 5) {
+            int unit = storey % 10;
+            if (unit < 5){
+                answer += unit;
+            }
+            else if (unit == 5){
+                int tenUnit = (storey / 10) % 10;
+                if (tenUnit >= 5){
                     storey += 10;
                 }
                 answer += 5;
-            } else {
-                answer += unit;  // 5 미만일 경우는 그대로 더함
+            }
+            else {
+                answer += 10 - unit;
+                storey += 10;
             }
             storey /= 10;
         }
